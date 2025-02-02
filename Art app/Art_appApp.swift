9a88f69552a9c7d9a538269a31f49f41b93cce90
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-02-02 21:03:37 +0300

    Initial Commit

---
+++ Art app/Art_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_appApp.swift
+//  Art app
+//
+//  Created by Marat Fakhrizhanov on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Art_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
